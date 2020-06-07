be947f3706a8890091624a4a6789c8618b0752a3
Ivalto <ivaltovianna@gmail.com>
2020-06-06 21:55:35 -0300

    Adicionado Ítens de Coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -51,4 +51,52 @@ function getCities(event) {
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities) // evento de mudança
+
+    // evento de mudança
+    .addEventListener("change", getCities)
+
+
+// Ítens de Coleta
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = [2, 3]
+
+function handleSelectedItem(event) {
+
+    const itemLi = event.target
+
+    // adicionar ou remover uma classe com js
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id
+
+    // verificar se existem itens selecionados
+    // se sim, pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    //  se já estiver selecionado, tirar da seleção
+    if (alreadySelected >= 0) {
+        //tirar da selação
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else {
+        //  se não estiver selecionado, adcionar a seleção
+        selectedItems.push(itemId)
+    }
+
+    //  atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+}
