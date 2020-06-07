99046428f75a2ce9b313677764cdc4e25e8f09ee
Ivalto <ivaltovianna@gmail.com>
2020-06-06 22:53:58 -0300

    Adicionado Pontos de coleta ao clicar em Pesquisar

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Ponto de coleta
+
+const buttonSearch = document.querySelector("#page-home main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+
+buttonSearch.addEventListener("click", () => {
+    modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide")
+})
